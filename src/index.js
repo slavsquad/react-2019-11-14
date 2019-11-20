e7f95c281fa9fa6072f4e9d7c1459e49a0929972
pirateway <mosro44an@yandex.ru>
2019-11-20 03:03:05 +0300

    Реализован компонет Restaurant, а так же вспомогательные компоненты Rating, Rater, Reviews, Review.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/app'
+import Rating from './components/rating'
 import {restaurants} from './fixtures'
+import Restaurants from './components/restaurants'
 
 ReactDOM.render(
   <App restaurants={restaurants} />,
